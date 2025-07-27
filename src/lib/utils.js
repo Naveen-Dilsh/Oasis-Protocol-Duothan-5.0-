cfe2b0687acc394f48f9579e26e1db128e155e64
Naveen-Dilsh <naveentype2001@gmail.com>
2025-07-27 23:51:58 +0530

    first commit

---
+++ src/lib/utils.js
@@ -0,0 +1,20 @@
+import { clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatDate(date) {
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(new Date(date))
+}
+
+export function generateId() {
+  return Math.random().toString(36).substring(2) + Date.now().toString(36)
+}
